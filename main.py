179f2428c914ca7b122cc751439a43310601e3b6
Lucrennah <104514956+Lucrennah@users.noreply.github.com>
2022-04-28 09:04:13 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from os import scandir
+import os
 
 route = './'
-l_archive = sorted([archive.name for archive in scandir(route) if archive.is_file()])
+l_archive = sorted([archive.name for archive in os.scandir(route) if archive.is_file()])
 
 
-def test():
-    for c in range(0, len(l_archive)):
-        b = "Episode {} = {}".format(c, l_archive[c])
-        print(b)
+def edit_media_data():
+    for f in range(0, len(l_archive[:-1])):
+        os.system(f"c:/users/baron/desktop/ffmpeg/bin/ffmpeg.exe -i '{l_archive[f]}' -codec copy -metadata title='' '{l_archive[f]}.new'")
+        os.system(f"rename '{l_archive[f]}.new' '{l_archive[f]}")
